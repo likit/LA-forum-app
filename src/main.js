4e1f17bce3ad6905dacfc420a518aa6c386e2949
Likit Preeyanon <likit.pre@mahidol.edu>
2020-09-13 00:10:45 +0700

    tried to use LIFF api
    added Buefy and fontawesome

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import liff from '@line/liff'
 
 Vue.config.productionTip = false
 Vue.prototype.$liff = liff
+Vue.use(Buefy)
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="section">
+    <div class="container">
+      <div class="box">
+        {{ lineName }}
+      </div>
+      <div class="box">
+        <b-field label="หมายเลขใบอนุญาตประกอบวิชาชีพ">
+          <b-input type="number"></b-input>
+        </b-field>
+        <b-field class="has-text-centered">
+          <a class="is-info button is-rounded">
+            <b-icon pack="far" icon="user"></b-icon>
+            <span>ลงทะเบียน</span>
+          </a>
+        </b-field>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: 'Register',
+  data() {
+    return {
+      lineName: ''
+    }
+  },
+  mounted() {
+    if (!this.$liff.isLoggedIn()) {
+      this.$liff.login()
+    }
   }
 }
 </script>
