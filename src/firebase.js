cf748a6f0e623bdecd45e88bb46233804aac83b8
Likit Preeyanon <likit.pre@mahidol.edu>
2020-09-12 23:12:22 +0700

    added liff app and firebase/firestore settings

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import * as firebase from "firebase/app";
+import 'firebase/auth'
+import 'firebase/firestore'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCqoGkV3iNOAyvhWm3TCgoONU9Lza6hBL8",
+    authDomain: "la-forum-2020.firebaseapp.com",
+    databaseURL: "https://la-forum-2020.firebaseio.com",
+    projectId: "la-forum-2020",
+    storageBucket: "la-forum-2020.appspot.com",
+    messagingSenderId: "166041537934",
+    appId: "1:166041537934:web:c1967c8278b7de4020a8df",
+    measurementId: "G-1EFPJEV2P3"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+
+export {
+    firebase as fb,
+    db,
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import liff from '@line/liff'
 
 Vue.config.productionTip = false
+Vue.prototype.$liff = liff
 
 new Vue({
   router,
